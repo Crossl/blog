775c9867a17238f72786f57519d5aa36d635d95a
Cross <cross@CrossdeMacBook-Pro.local>
2018-07-09 22:53:39 +0800

    update

--- demo.js
+++ demo.js
@@ -13,7 +13,7 @@ fs.mkdirSync('js') // mkdir js
 
 fs.writeFileSync("./index.html", "<!DOCTYPE><title>Hello</title><h1>Hi</h1>")
 fs.writeFileSync("css/style.css", 'h1{color: red;}')
-fs.writeFileSync("./js/main.js", "Hello World")
+fs.writeFileSync("./js/main.js", 'var string = "Hello World alert(string)')
 
  process.exit(0)
     }
